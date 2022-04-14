4addc28ba17c281c04e3f20c231b6e60018ff8f4
Johnathan Viduchinsky <Johnathan.V@quali.com>
2022-04-14 23:50:06 +0300

    adds simple terraform module

---
+++ terraform/simple-tf-module/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 0.13"
+//  backend "local" {
+//    path    = "mybackend/terraform.tfstate"
+//  }
+}
+
+// Shell
+resource "null_resource" "task_log" {
+  provisioner "local-exec" {
+    command = "echo task_log. variables: rabbit_endpoint=bla."
+  }
+}
+
+resource "null_resource" "task_long" {
+  provisioner "local-exec" {
+    command = "echo \"long task start\" && sleep 5 && echo \"long task end\""
+  }
+}
+
+locals {
+  key1 = "somekey"
+}
